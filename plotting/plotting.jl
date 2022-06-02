c0eef33a68c3f8763b155cb6e3befd0db1b19371
Liam Kruse <lamk98@gmail.com>
2022-06-01 17:37:11 -0700

    Added plot outputs

--- plotting/plotting.jl
+++ plotting/plotting.jl
@@ -31,6 +31,8 @@ define_color("cordovan_light", [183,124,126])
 define_color("salmon", [242,109,113])
 
 define_color("middle_green", [91,140,90])
+define_color("middle_light", [140,175,140])
+
 define_color("honey", [232, 174, 104])
 
 define_color("fire_opal", [223,96,71])
@@ -52,6 +54,7 @@ function interp_data(file, arr)
 end
 
 # tensorboard --logdir ./hopper
+# tensorboard --logdir logs/hopper
 ##
 ################################################################################
 # HOPPER
@@ -60,7 +63,6 @@ end
 #*******************************************************************************
 # Rewards
 #*******************************************************************************
-# // TODO: Make this more flexible
 domain = 200:100:90000
 files = readdir("hopper_rew/")
 aggregate = []
@@ -69,7 +71,6 @@ for f in files
     push!(aggregate, interp_arr)
 end
 
-##
 μ_arr = []; μ_plus_σ = []; μ_minus_σ = []; interaction = []
 for i = 1:length(domain)
     step = [x[i] for x in aggregate]
@@ -83,22 +84,100 @@ end
 df = DataFrame(interaction = interaction, mean = μ_arr, upper = μ_plus_σ, lower = μ_minus_σ)
 CSV.write("savedata/hopper_rew.csv", df)
 
-##
 pushPGFPlotsPreamble("\\usepgfplotslibrary{fillbetween}")
 p = Axis([
     Plots.Command("\\addplot [name path = A, vandeusen, opacity = 0.0, forget plot] table [x = {interaction}, y = {lower}, col sep=comma] {savedata/hopper_rew.csv}"),
     Plots.Command("\\addplot [name path = B, vandeusen, opacity = 0.0, forget plot] table [x = {interaction}, y = {upper}, col sep=comma] {savedata/hopper_rew.csv}"),
     Plots.Command("\\addplot [vandeusen, very thick] table [x = {interaction}, y = {mean}, col sep=comma] {savedata/hopper_rew.csv}; \\addlegendentry{Mean reward}"),
     Plots.Command("\\addplot [vandeusen_light,opacity = 0.5] fill between [of = A and B]; \\addlegendentry{\$2\\sigma\$ error bounds}"),
-#    Plots.Command("\\addplot [mark=none, cordovan, very thick, domain=0:8.2] {1.0}; \\addlegendentry{\$c\$}"),
 ],
 style="enlarge x limits=false,grid=both",legendPos = "north west", legendStyle = "{nodes={scale=0.75}}",
 #ylabel=L"$\hat{c}$", xlabel="Interaction",
 ylabel="Mean Reward", xlabel="Interaction",
 title="Average Reward per Episode (Hopper)")
 save("hopper_rew.pdf", p)
+save("hopper_rew.tex", p, include_preamble=false)
+
+##
+#*******************************************************************************
+# Actor
+#*******************************************************************************
+domain = 300:100:90000
+files = readdir("hopper_actor/")
+aggregate = []
+for f in files
+    interp_arr = interp_data("hopper_actor/"*f, domain)
+    push!(aggregate, interp_arr)
+end
+
+μ_arr = []; μ_plus_σ = []; μ_minus_σ = []; interaction = []
+for i = 1:length(domain)
+    step = [x[i] for x in aggregate]
+    μ = mean(step); σ = std(step)
+    push!(interaction, domain[i])
+    push!(μ_arr, μ)
+    push!(μ_plus_σ, μ + σ)
+    push!(μ_minus_σ, μ - σ)
+end
+
+df = DataFrame(interaction = interaction, mean = μ_arr, upper = μ_plus_σ, lower = μ_minus_σ)
+CSV.write("savedata/hopper_actor.csv", df)
+
+pushPGFPlotsPreamble("\\usepgfplotslibrary{fillbetween}")
+p = Axis([
+    Plots.Command("\\addplot [name path = A, cordovan, opacity = 0.0, forget plot] table [x = {interaction}, y = {lower}, col sep=comma] {savedata/hopper_actor.csv}"),
+    Plots.Command("\\addplot [name path = B, cordovan, opacity = 0.0, forget plot] table [x = {interaction}, y = {upper}, col sep=comma] {savedata/hopper_actor.csv}"),
+    Plots.Command("\\addplot [cordovan, very thick] table [x = {interaction}, y = {mean}, col sep=comma] {savedata/hopper_actor.csv}; \\addlegendentry{Actor loss}"),
+    Plots.Command("\\addplot [cordovan_light,opacity = 0.5] fill between [of = A and B]; \\addlegendentry{\$2\\sigma\$ error bounds}"),
+],
+style="enlarge x limits=false,grid=both",legendPos = "north east", legendStyle = "{nodes={scale=0.75}}",
+#ylabel=L"$\hat{c}$", xlabel="Interaction",
+ylabel="Actor Loss", xlabel="Interaction",
+title="Actor Loss (Hopper)")
+save("hopper_actor.pdf", p)
+save("hopper_actor.tex", p,include_preamble=false)
+
+##
+#*******************************************************************************
+# Critic
+#*******************************************************************************
+domain = 300:100:90000
+files = readdir("hopper_critic/")
+aggregate = []
+for f in files
+    interp_arr = interp_data("hopper_critic/"*f, domain)
+    push!(aggregate, interp_arr)
+end
+
+μ_arr = []; μ_plus_σ = []; μ_minus_σ = []; interaction = []
+for i = 1:length(domain)
+    step = [x[i] for x in aggregate]
+    μ = mean(step); σ = std(step)
+    push!(interaction, domain[i])
+    push!(μ_arr, μ)
+    push!(μ_plus_σ, μ + σ)
+    push!(μ_minus_σ, μ - σ)
+end
+
+df = DataFrame(interaction = interaction, mean = μ_arr, upper = μ_plus_σ, lower = μ_minus_σ)
+CSV.write("savedata/hopper_critic.csv", df)
+
+pushPGFPlotsPreamble("\\usepgfplotslibrary{fillbetween}")
+p = Axis([
+    Plots.Command("\\addplot [name path = A, middle_green, opacity = 0.0, forget plot] table [x = {interaction}, y = {lower}, col sep=comma] {savedata/hopper_critic.csv}"),
+    Plots.Command("\\addplot [name path = B, middle_green, opacity = 0.0, forget plot] table [x = {interaction}, y = {upper}, col sep=comma] {savedata/hopper_critic.csv}"),
+    Plots.Command("\\addplot [middle_green, very thick] table [x = {interaction}, y = {mean}, col sep=comma] {savedata/hopper_critic.csv}; \\addlegendentry{Critic loss}"),
+    Plots.Command("\\addplot [middle_light,opacity = 0.5] fill between [of = A and B]; \\addlegendentry{\$2\\sigma\$ error bounds}"),
+],
+style="enlarge x limits=false,grid=both",legendPos = "north west", legendStyle = "{nodes={scale=0.75}}",
+#ylabel=L"$\hat{c}$", xlabel="Interaction",
+ylabel="Critic Loss", xlabel="Interaction",
+title="Critic Loss (Hopper)")
+save("hopper_critic.pdf", p)
+save("hopper_critic.tex", p, include_preamble=false)
 
 # tensorboard --logdir ./walker2d
+# tensorboard --logdir logs/walker2d
 ##
 ################################################################################
 # WALKER2D
@@ -107,7 +186,6 @@ save("hopper_rew.pdf", p)
 #*******************************************************************************
 # Rewards
 #*******************************************************************************
-# // TODO: Make this more flexible
 domain = 200:100:90000
 files = readdir("walker2d_rew/")
 aggregate = []
@@ -116,7 +194,6 @@ for f in files
     push!(aggregate, interp_arr)
 end
 
-##
 μ_arr = []; μ_plus_σ = []; μ_minus_σ = []; interaction = []
 for i = 1:length(domain)
     step = [x[i] for x in aggregate]
@@ -130,37 +207,98 @@ end
 df = DataFrame(interaction = interaction, mean = μ_arr, upper = μ_plus_σ, lower = μ_minus_σ)
 CSV.write("savedata/walker2d_rew.csv", df)
 
-##
 pushPGFPlotsPreamble("\\usepgfplotslibrary{fillbetween}")
 p = Axis([
-    Plots.Command("\\addplot [name path = A, cordovan, opacity = 0.0, forget plot] table [x = {interaction}, y = {lower}, col sep=comma] {savedata/walker2d_rew.csv}"),
-    Plots.Command("\\addplot [name path = B, cordovan, opacity = 0.0, forget plot] table [x = {interaction}, y = {upper}, col sep=comma] {savedata/walker2d_rew.csv}"),
-    Plots.Command("\\addplot [cordovan, very thick] table [x = {interaction}, y = {mean}, col sep=comma] {savedata/walker2d_rew.csv}; \\addlegendentry{Mean reward}"),
-    Plots.Command("\\addplot [cordovan_light,opacity = 0.5] fill between [of = A and B]; \\addlegendentry{\$2\\sigma\$ error bounds}"),
-#    Plots.Command("\\addplot [mark=none, cordovan, very thick, domain=0:8.2] {1.0}; \\addlegendentry{\$c\$}"),
+    Plots.Command("\\addplot [name path = A, vandeusen, opacity = 0.0, forget plot] table [x = {interaction}, y = {lower}, col sep=comma] {savedata/walker2d_rew.csv}"),
+    Plots.Command("\\addplot [name path = B, vandeusen, opacity = 0.0, forget plot] table [x = {interaction}, y = {upper}, col sep=comma] {savedata/walker2d_rew.csv}"),
+    Plots.Command("\\addplot [vandeusen, very thick] table [x = {interaction}, y = {mean}, col sep=comma] {savedata/walker2d_rew.csv}; \\addlegendentry{Mean reward}"),
+    Plots.Command("\\addplot [vandeusen_light,opacity = 0.5] fill between [of = A and B]; \\addlegendentry{\$2\\sigma\$ error bounds}"),
 ],
 style="enlarge x limits=false,grid=both",legendPos = "north west", legendStyle = "{nodes={scale=0.75}}",
 #ylabel=L"$\hat{c}$", xlabel="Interaction",
 ylabel="Mean Reward", xlabel="Interaction",
-title="Average Reward per Episode (Walker2D)")
+title="Average Reward per Episode (Walker2d)")
 save("walker2d_rew.pdf", p)
-
+save("walker2d_rew.tex", p, include_preamble=false)
 
 ##
-# TOGETHER
+#*******************************************************************************
+# Actor
+#*******************************************************************************
+domain = 500:100:90000
+files = readdir("walker2d_actor/")
+aggregate = []
+for f in files
+    interp_arr = interp_data("walker2d_actor/"*f, domain)
+    push!(aggregate, interp_arr)
+end
+
+μ_arr = []; μ_plus_σ = []; μ_minus_σ = []; interaction = []
+for i = 1:length(domain)
+    step = [x[i] for x in aggregate]
+    μ = mean(step); σ = std(step)
+    push!(interaction, domain[i])
+    push!(μ_arr, μ)
+    push!(μ_plus_σ, μ + σ)
+    push!(μ_minus_σ, μ - σ)
+end
+
+df = DataFrame(interaction = interaction, mean = μ_arr, upper = μ_plus_σ, lower = μ_minus_σ)
+CSV.write("savedata/walker2d_actor.csv", df)
+
 pushPGFPlotsPreamble("\\usepgfplotslibrary{fillbetween}")
 p = Axis([
-    Plots.Command("\\addplot [name path = A, cordovan, opacity = 0.0, forget plot] table [x = {interaction}, y = {lower}, col sep=comma] {savedata/walker2d_rew.csv}"),
-    Plots.Command("\\addplot [name path = B, cordovan, opacity = 0.0, forget plot] table [x = {interaction}, y = {upper}, col sep=comma] {savedata/walker2d_rew.csv}"),
-    Plots.Command("\\addplot [cordovan, very thick] table [x = {interaction}, y = {mean}, col sep=comma] {savedata/walker2d_rew.csv}; \\addlegendentry{Walker2D mean reward}"),
-    Plots.Command("\\addplot [cordovan_light,opacity = 0.5] fill between [of = A and B]; \\addlegendentry{Walker2D \$2\\sigma\$ error bounds}"),
-    Plots.Command("\\addplot [name path = A, vandeusen, opacity = 0.0, forget plot] table [x = {interaction}, y = {lower}, col sep=comma] {savedata/hopper_rew.csv}"),
-    Plots.Command("\\addplot [name path = B, vandeusen, opacity = 0.0, forget plot] table [x = {interaction}, y = {upper}, col sep=comma] {savedata/hopper_rew.csv}"),
-    Plots.Command("\\addplot [vandeusen, very thick] table [x = {interaction}, y = {mean}, col sep=comma] {savedata/hopper_rew.csv}; \\addlegendentry{Hopper mean reward}"),
-    Plots.Command("\\addplot [vandeusen_light,opacity = 0.5] fill between [of = A and B]; \\addlegendentry{Hopper \$2\\sigma\$ error bounds}"),
+    Plots.Command("\\addplot [name path = A, cordovan, opacity = 0.0, forget plot] table [x = {interaction}, y = {lower}, col sep=comma] {savedata/walker2d_actor.csv}"),
+    Plots.Command("\\addplot [name path = B, cordovan, opacity = 0.0, forget plot] table [x = {interaction}, y = {upper}, col sep=comma] {savedata/walker2d_actor.csv}"),
+    Plots.Command("\\addplot [cordovan, very thick] table [x = {interaction}, y = {mean}, col sep=comma] {savedata/walker2d_actor.csv}; \\addlegendentry{Actor loss}"),
+    Plots.Command("\\addplot [cordovan_light,opacity = 0.5] fill between [of = A and B]; \\addlegendentry{\$2\\sigma\$ error bounds}"),
 ],
-style="enlarge x limits=false,grid=both",legendPos = "north west", legendStyle = "{nodes={scale=0.65}}",
+style="enlarge x limits=false,grid=both",legendPos = "north west", legendStyle = "{nodes={scale=0.75}}",
 #ylabel=L"$\hat{c}$", xlabel="Interaction",
-ylabel="Mean Reward", xlabel="Interaction",
-title="Average Reward per Episode")
-save("together.pdf", p)
+ylabel="Actor Loss", xlabel="Interaction",
+title="Actor Loss (Walker2d)")
+save("walker2d_actor.pdf", p)
+save("walker2d_actor.tex", p,include_preamble=false)
+
+##
+#*******************************************************************************
+# Critic
+#*******************************************************************************
+domain = 500:100:90000
+files = readdir("walker2d_critic/")
+aggregate = []
+for f in files
+    interp_arr = interp_data("walker2d_critic/"*f, domain)
+    push!(aggregate, interp_arr)
+end
+
+μ_arr = []; μ_plus_σ = []; μ_minus_σ = []; interaction = []
+for i = 1:length(domain)
+    step = [x[i] for x in aggregate]
+    μ = mean(step); σ = std(step)
+    push!(interaction, domain[i])
+    push!(μ_arr, μ)
+    push!(μ_plus_σ, μ + σ)
+    push!(μ_minus_σ, μ - σ)
+end
+
+df = DataFrame(interaction = interaction, mean = μ_arr, upper = μ_plus_σ, lower = μ_minus_σ)
+CSV.write("savedata/walker2d_critic.csv", df)
+
+pushPGFPlotsPreamble("\\usepgfplotslibrary{fillbetween}")
+p = Axis([
+    Plots.Command("\\addplot [name path = A, middle_green, opacity = 0.0, forget plot] table [x = {interaction}, y = {lower}, col sep=comma] {savedata/walker2d_critic.csv}"),
+    Plots.Command("\\addplot [name path = B, middle_green, opacity = 0.0, forget plot] table [x = {interaction}, y = {upper}, col sep=comma] {savedata/walker2d_critic.csv}"),
+    Plots.Command("\\addplot [middle_green, very thick] table [x = {interaction}, y = {mean}, col sep=comma] {savedata/walker2d_critic.csv}; \\addlegendentry{Critic loss}"),
+    Plots.Command("\\addplot [middle_light,opacity = 0.5] fill between [of = A and B]; \\addlegendentry{\$2\\sigma\$ error bounds}"),
+],
+style="enlarge x limits=false,grid=both",legendPos = "north west", legendStyle = "{nodes={scale=0.75}}",
+#ylabel=L"$\hat{c}$", xlabel="Interaction",
+ylabel="Critic Loss", xlabel="Interaction",
+title="Critic Loss (Walker2d)")
+save("walker2d_critic.pdf", p)
+save("walker2d_critic.tex", p, include_preamble=false)
+
+##
+p = Plots.BarChart(["a", "b", "c"], [2,3,5], errorBars=ErrorBars(y=[0.5,0.7,1.5]))
+save("marc_code.tex", p,include_preamble=false)
