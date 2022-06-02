11699a82ed4e66b7e3795d9ef5bcbde794e4b6cb
Marc Schlichting <schlichting.marc@gmail.com>
2022-06-02 10:06:07 -0700

    all csvs from original experiments

--- run_experiments.py
+++ run_experiments.py
@@ -4,113 +4,152 @@ from datetime import datetime
 # test run
 # os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(0))
 
-#prio 1
-EXP_ID = 1
+# #prio 1
+# EXP_ID = 1
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 2
+# EXP_ID = 2
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 3
+# EXP_ID = 3
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 4
+# EXP_ID = 4
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 5
+# EXP_ID = 7
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 6
+# EXP_ID = 8
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 7
+# EXP_ID = 9
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 8
+# EXP_ID = 10
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 9
+# EXP_ID = 13
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 10
+# EXP_ID = 15
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 11
+# EXP_ID = 17
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 12
+# EXP_ID = 19
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 13
+# EXP_ID = 21
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 14
+# EXP_ID = 22
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 15
+# EXP_ID = 14
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 16
+# EXP_ID = 16
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 17
+# EXP_ID = 18
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 18
+# EXP_ID = 20
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 19
+# EXP_ID = 5
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 20
+# EXP_ID = 6
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 21
+# EXP_ID = 11
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+# #prio 22
+# EXP_ID = 12
+# print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
+
+#prio 23
+EXP_ID = 23
 print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
 
-#prio 2
-EXP_ID = 2
+#prio 24
+EXP_ID = 25
 print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
 
-#prio 3
-EXP_ID = 3
+#prio 25
+EXP_ID = 27
 print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
 
-#prio 4
-EXP_ID = 4
+#prio 26
+EXP_ID = 29
 print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
 
-#prio 5
-EXP_ID = 7
+#prio 27
+EXP_ID = 24
 print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
 
-#prio 6
-EXP_ID = 8
+#prio 28
+EXP_ID = 26
 print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
 
-#prio 7
-EXP_ID = 9
+#prio 29
+EXP_ID = 28
 print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
 
-#prio 8
-EXP_ID = 10
+#prio 30
+EXP_ID = 30
 print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
-
-#prio 9
-EXP_ID = 13
-print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
-
-#prio 10
-EXP_ID = 15
-print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
-
-#prio 11
-EXP_ID = 17
-print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
-
-#prio 12
-EXP_ID = 19
-print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
-
-#prio 13
-EXP_ID = 21
-print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
-
-#prio 14
-EXP_ID = 22
-print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
-
-#prio 15
-EXP_ID = 14
-print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
-
-#prio 16
-EXP_ID = 16
-print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
-
-#prio 17
-EXP_ID = 18
-print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
-
-#prio 18
-EXP_ID = 20
-print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
-
-#prio 19
-EXP_ID = 5
-print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
-
-#prio 20
-EXP_ID = 6
-print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
-
-#prio 21
-EXP_ID = 11
-print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
-
-#prio 22
-EXP_ID = 12
-print("Experiment {} start time: ".format(EXP_ID), datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-os.system("python3 ./experiments/{0}/training_{0}.py 2>&1 | tee ./experiments/{0}/log_{0}.txt".format(EXP_ID))
-
